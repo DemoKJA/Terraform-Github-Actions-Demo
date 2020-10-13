6fd235029907963960012e7dce0235cabf77eba0
Kimiebi Akah <kimiebi.akah@insight.com>
2020-10-13 12:53:49 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,109 @@
+# Configure the Microsoft Azure Provider.
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "kjTEST"
+  location = "eastus 2"
+}
+
+
+# Create Azure Analysis Services
+resource "azurerm_analysis_services_server" "analysisserver" {
+  name                    = "kjaaastest"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku                     = "S0"
+  enable_power_bi_service = true
+  
+  ipv4_firewall_rule {
+    name        = "myRule1"
+    range_start = "0.0.0.0"
+    range_end   = "255.255.255.255"
+  }
+  
+}
+
+
+# Create Azure Datafactory
+resource "azurerm_data_factory" "adf" {
+  name                = "kjADFtest"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+# BELOW IS USED TO CREATE A SYNAPSE POOL, TODD WALKER NOTED WE MAY BE ABLE TO SETUP ONE MANUALLU WITH THE TEAM
+/*
+
+#** Storage account ** will most likely replace with references to existing storage accounts
+resource "azurerm_storage_account" "storage" {
+  name                     = "kjastoragetest"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+#** Setting the role to permit the filestore creation
+resource "azurerm_role_assignment" "role" {
+	scope                = azurerm_resource_group.rg.id
+	role_definition_name = "Storage Blob Data Contributor"
+    principal_id         = "0b2df119-bc75-4148-a92d-b1a5c4132a7a"  #** Should be the service Principal on the resource groun and take ObjectID from Kroger to place here
+}
+
+# File system
+resource "azurerm_storage_data_lake_gen2_filesystem" "filesystem" {
+  name               = "testfilesystem"
+  storage_account_id = azurerm_storage_account.storage.id
+  depends_on = [azurerm_role_assignment.role]  # dependency for the role created
+}
+
+
+# Synapse 
+resource "azurerm_synapse_workspace" "workspace" {
+  name                                 = "kjaworkspace"
+  resource_group_name                  = azurerm_resource_group.rg.name
+  location                             = azurerm_resource_group.rg.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.filesystem.id
+  sql_administrator_login              = "kjakah08"
+  sql_administrator_login_password     = "123456test!"
+}
+
+# Firewall rule to allow all ** May want to change before sending to Kroger...
+resource "azurerm_synapse_firewall_rule" "example" {
+  name                 = "AllowAll"
+  synapse_workspace_id = azurerm_synapse_workspace.workspace.id
+  start_ip_address     = "0.0.0.0"
+  end_ip_address       = "255.255.255.255"
+}
+
+# 
+resource "azurerm_synapse_sql_pool" "synapsepool" {
+  name                 = "kjatestsqlpool"
+  synapse_workspace_id = azurerm_synapse_workspace.workspace.id
+  sku_name             = "DW100c"
+  create_mode          = "Default"
+  
+  timeouts {
+    create = "1h"
+    delete = "1h"
+	update = "1h"
+  }
+}
+
+*/
