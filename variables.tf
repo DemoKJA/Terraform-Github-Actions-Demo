c75840ac6f81628f0bb930a1c8203e20db5f771a
Kimiebi Akah <kimiebi.akah@insight.com>
2020-10-14 16:59:44 -0400

    ok

--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
 
 variable "sub" { 
   type = string 
-  default = "23b3554b-c081-418d-b674-b21c29fa1a13"
 }
 
 
 variable "client_id" { 
   type = string 
-  default = "f8d4a41e-70af-4d95-b4f0-9d9df836508d"
 }
 
 
 variable "tenant_id" { 
   type = string 
-  default = "6c637512-c417-4e78-9d62-b61258e4b619"
 }
 
 variable "client_secret" { 
