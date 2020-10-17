8e79ca84d0564e6a84ff5c633a59c4fb4f9c3987
kjakah <48288701+kjakah@users.noreply.github.com>
2020-10-16 22:18:38 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  version = "2.31.1"
+  subscription_id = var.sub
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+  features {}
+}
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "kj-rg"
