76e99038178416aef6332d9dd75277a5fd9856a7
kjakah <48288701+kjakah@users.noreply.github.com>
2020-10-16 22:20:43 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,18 +1,10 @@
-provider "azurerm" {
-  version = "2.31.1"
-  subscription_id = var.sub
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
-  features {}
-}
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "kj-rg"
     storage_account_name = "kjastorage"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    secret_key           = "qLmeOPsUrdUvacz8pR1AsYML0Tg2pMg4lCqyik3fH2qHVN3RqRY6hgOWgROJbvaAzs7vRSAL5f9ruxHqRmIFLg=="
   }
 }
 
