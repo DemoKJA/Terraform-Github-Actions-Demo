752fbff98a9e5f0dcca05d596593b61da042d76d
kjakah <48288701+kjakah@users.noreply.github.com>
2020-10-16 22:24:26 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "kjastorage"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    secret_key           = "qLmeOPsUrdUvacz8pR1AsYML0Tg2pMg4lCqyik3fH2qHVN3RqRY6hgOWgROJbvaAzs7vRSAL5f9ruxHqRmIFLg=="
+    access_key           = ARM_ACCESS_KEY
   }
 }
 
