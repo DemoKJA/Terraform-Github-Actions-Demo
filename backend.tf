1598ccdbbb15e423c0967d3c5fc3322772da4a87
kjakah <48288701+kjakah@users.noreply.github.com>
2020-10-16 23:55:50 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,12 @@
+# *Authentication managed with environmental variables pulled from github secrets:
+# ARM_TENANT_ID
+# ARM_SUBSCRIPTION_ID
+# ARM_CLIENT_ID
+# ARM_SUBSCRIPTION_ID
+
+# *Secret for storage account managed with environmental variables pulled from github secrets:
+# ARM_ACCESS_KEY
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "kj-rg"
