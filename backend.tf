7ceb5bc1cb261d3aa3515f6396e82b2d3ebaaee2
kjakah <48288701+kjakah@users.noreply.github.com>
2020-10-16 21:51:21 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "kj-rg"
+    storage_account_name = "kjastorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    access_key           = "qLmeOPsUrdUvacz8pR1AsYML0Tg2pMg4lCqyik3fH2qHVN3RqRY6hgOWgROJbvaAzs7vRSAL5f9ruxHqRmIFLg=="
+  }
+}
