544c5757734e8da066e30d578061481b511be7a7
Kimiebi Akah <kimiebi.akah@insight.com>
2020-10-22 16:46:16 -0400

    remove

--- main.tf
+++ main.tf
@@ -6,12 +6,10 @@
 
 
 # Create a resource groups
-resource "azurerm_resource_group" "rg" {
-  name     = var.prefix
-  location = var.location
-}
-
-
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.prefix
+#   location = var.location
+# }
 
 
 # # Create Azure Analysis Services
