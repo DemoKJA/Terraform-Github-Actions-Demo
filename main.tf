3f1974a793a63c7b85b020e610998c78b89e4c20
Kimiebi Akah <kimiebi.akah@insight.com>
2020-12-09 18:09:47 -0500

    testing parameterizing the naming

--- main.tf
+++ main.tf
@@ -33,6 +33,9 @@ resource "azurerm_resource_group_template_deployment" "templateTEST" {
   resource_group_name = azurerm_resource_group.rg.name
   deployment_mode     = "Incremental" # If set to "Complete", will blow away everything in the resource group that's not in the ARM template
   template_content    = file("${path.module}/arm/createLogicAppsTEST.json")
+  parameters = {
+    "logicAppName" = "logic-${var.prefix}-"
+  }
 
 }
 
