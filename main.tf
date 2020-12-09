6f11be02cc270a04a98fa6e7879142ce3854091f
Kimiebi Akah <kimiebi.akah@insight.com>
2020-12-09 17:41:34 -0500

    just server

--- main.tf
+++ main.tf
@@ -28,13 +28,13 @@ resource "azurerm_sql_server" "sqlserver" {
 #     "type": "SystemAssigned"
 # },
 
-resource "azurerm_resource_group_template_deployment" "templateTEST" {
-  name                = "arm-Deployment"
-  resource_group_name = azurerm_resource_group.rg.name
-  deployment_mode     = "Incremental" # If set to "Complete", will blow away everything in the resource group that's not in the ARM template
-  template_content    = file("${path.module}/arm/createLogicAppsTEST.json")
+# resource "azurerm_resource_group_template_deployment" "templateTEST" {
+#   name                = "arm-Deployment"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   deployment_mode     = "Incremental" # If set to "Complete", will blow away everything in the resource group that's not in the ARM template
+#   template_content    = file("${path.module}/arm/createLogicAppsTEST.json")
 
-}
+# }
 
 
 # # Then create a datafactory
