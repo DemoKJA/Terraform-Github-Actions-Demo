e8e4c932525821ce2615e8e316026f888c14ef5f
Kimiebi Akah <kimiebi.akah@insight.com>
2021-01-07 12:31:45 -0500

    dynamic branches

--- main.tf
+++ main.tf
@@ -107,10 +107,15 @@ resource "azurerm_data_factory" "adf" {
     type = "SystemAssigned"
   }
 
+
+  # For "branch_name", it's the branch where actual version and work is done, note the 'main' branch, 
+  # which is what will be used to publich ARM template to envionrmenets in terraform.
+  # For handling different tiers, ie. dev,stage, prod, paramterize their own branch, see below, but publish from
+  # only the tier you want to then be published in terraform 
   github_configuration {
     account_name    = "demokja"
     git_url         = "https://github.com"
-    branch_name     = "holder-colab" # Branch where actual version and work is done, note the 'main', which is what will be used to publich ARM template to
+    branch_name     = "holder-colab-${var.prefix}"
     repository_name = "Terraform-Github-Actions-Demo"
     root_folder     = "/ADF-ARM"
   }
