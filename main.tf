cd90be6c89c20ea1affe16816aaf552343aadb17
Kimiebi Akah <kjakah08@gmail.com>
2021-04-28 15:43:06 -0400

    rename tags

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group_template_deployment" "managed-instance" {
         "value": "eastus2"
     },
     "managedInstanceTags": {
-        "value": "${va.tags}"
+        "value": "${var.tags}"
     },
     "skuName": {
         "value": "GP_Gen5"
